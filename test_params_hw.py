5b4ea35ddf98c741dc2e2613231def47026d9c5f
Amianda <lil888@mail.ru>
2022-04-15 17:43:41 +0200

    add files after error

--- test_params_hw.py
+++ test_params_hw.py
@@ -32,37 +32,3 @@ def test_enter_the_answer(browser, number):
 
     time.sleep(3)
 
-import pytest
-import time
-import math
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
-@pytest.mark.parametrize('number', ["895", "896", "897", "898", "899", "903", "904", "905", "905"])
-def test_enter_the_answer(browser, number):
-    link = f"https://stepik.org/lesson/236{number}/step/1/"
-    browser.get(link)
-    answer = math.log(int(time.time()))
-    answer_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".textarea")))
-    answer_input.send_keys(answer)
-
-    browser.find_element(By.CSS_SELECTOR, ".submit-submission").click()
-
-    correct_mark = WebDriverWait(browser, 3).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".smart-hints__hint")))
-    assert correct_mark.text == "Correct!", "The answer is not correct"
-
-    time.sleep(3)
-
--- test_registration.py
+++ test_registration.py
@@ -19,24 +19,3 @@ try:
 finally:
     time.sleep(5)
     browser.quit()
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-
-link_1 = "http://suninjuly.github.io/registration1.html"
-link_2 = "http://suninjuly.github.io/registration2.html"
-
-try:
-    browser = webdriver.Chrome()  # поменять, если не используется chromedriver!
-    browser.get(link_2)
-
-    browser.find_element(By.CSS_SELECTOR, '.first_block .first').send_keys('Lilia')
-    browser.find_element(By.CSS_SELECTOR, '.first_block .second').send_keys('Vas')
-    browser.find_element(By.CSS_SELECTOR, '.third_class .third').send_keys('vas@google.com')
-
-    browser.find_element(By.CSS_SELECTOR, "button.btn").click()
-
-
-finally:
-    time.sleep(5)
-    browser.quit()
